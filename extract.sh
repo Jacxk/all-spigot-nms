1f04fcafe195f22f0c1bb62eafeeeab1ab74961f
Yanluis Fermin <tfermin1827@hotmail.com>
2019-07-15 21:15:54 -0700

    Cleaned command

--- extract.sh
+++ extract.sh
@@ -4,11 +4,13 @@
 
 holder="extracted_files"
 mkdir "$holder" -p
+
 echo "Extracting all spigot.jar files, please wait..."
+cd "$holder"/
 for filename in /spigot_versions/spigot-1.*.jar; do
-	cd "$holder"/
-	unzip -uoq ../"$filename" 'org/bukkit/craftbukkit/**' -x "META-INF/*"
-	unzip -uoq ../"$filename" 'net/minecraft/server/**' -x "META-INF/*"
-	cd ..
+    unzip -uoq ../"$filename" 'org/bukkit/craftbukkit/**' -x "META-INF/*"
+    unzip -uoq ../"$filename" 'net/minecraft/server/**' -x "META-INF/*"
 done
+cd ..
+
 echo "Extract completed!"
