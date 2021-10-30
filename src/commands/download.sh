004fbe88ac2d70fe2b37d1edb2870cdae882f6be
Yanluis Fermin <tfermin1827@hotmail.com>
2021-10-30 11:42:29 -0400

    Fixed issue #3

--- src/commands/download.sh
+++ src/commands/download.sh
@@ -3,7 +3,7 @@
 # Run this to download all the available jars.
 
 # All the NMS versions available
-versions=(
+legacy_versions=(
   "spigot-1.8-R0.1-SNAPSHOT-latest"
   "spigot-1.8.3-R0.1-SNAPSHOT-latest"
   "spigot-1.8.8-R0.1-SNAPSHOT-latest"
@@ -22,15 +22,33 @@ versions=(
   "spigot-1.17.1"
 )
 
+versions=(
+  "spigot-1.17.1"
+)
+
 # Go back and create dir spigot_versions if not exists
 cd ../../
 mkdir spigot_versions/ -p
 cd spigot_versions/ || exit 1
 
-# Download all versions from getbukkit.org on dir spigot_versions
-for version in "${versions[@]}"; do
+# Download all legacy versions from cdn.getbukkit.org on dir spigot_versions
+for version in "${legacy_versions[@]}"; do
   echo -e "Downloading \e[34m$version\e[39m...\e[39m"
   if curl --fail https://cdn.getbukkit.org/spigot/"$version".jar --output "$version".jar; then
+    echo -e "Downloaded \e[34m$version \e[39msuccessfully!"
+  else
+    # If the download fails stop the execution
+    echo ""
+    echo -e "\e[91mSomething went wrong!\e[39m"
+    exit 1
+  fi
+  echo ""
+done
+
+# Download all versions from download.getbukkit.org on dir spigot_versions
+for version in "${versions[@]}"; do
+  echo -e "Downloading \e[34m$version\e[39m...\e[39m"
+  if curl --fail https://download.getbukkit.org/spigot/"$version".jar --output "$version".jar; then
     echo -e "Downloaded \e[34m$version \e[39msuccessfully!"
   else
     # If the download fails stop the execution
